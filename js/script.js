45b0bf0708920dde1203202e8d9027026115209c
Magdalena Falkowska-Legieć <magda.f41@gmail.com>
2020-11-19 10:59:36 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const price = document.querySelector('#price');
+const people = document.querySelector('#people');
+const tip = document.querySelector('#tip');
+const error = document.querySelector('.error');
+const countBtn = document.querySelector('.count');
+const costInfo = document.querySelector('.cost-info');
+const cost = document.querySelector('.cost');
+
+
+const showBill = () => {
+
+
+
+    if (price.value == '' || people.value == '' || tip.value == 0) {
+        error.textContent = 'Uzupełnij wszystkie pola';
+        costInfo.style.display = "none";
+    } else {
+        error.textContent = '';
+        countBill();
+
+    }
+};
+
+const countBill = () => {
+    const newPrice = parseFloat(price.value);
+    const newPeople = parseInt(people.value);
+    const newTip = parseFloat(tip.value);
+
+    console.log(newPrice);
+    console.log(newPeople);
+    console.log(newTip);
+    const sum = (newPrice + (newTip * newPrice)) / newPeople;
+
+    costInfo.style.display = "block";
+
+    cost.textContent = sum.toFixed(2)
+
+    console.log(sum);
+}
+
+
+countBtn.addEventListener('click', showBill);
